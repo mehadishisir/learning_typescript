826a28ca97bd9bb4594cd39634c0b277edd32fb0
mehadishisir <mehadishisir@gmail.com>
2026-05-01 22:16:47 +0600

    explore tuple

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -9,4 +9,9 @@ mixedArray.push("apple"); // Valid
 mixedArray.push(7);
 mixedArray.push(true); // Error: Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
 
-// object
+// tuple
+let tuple: [string, number] = ["hello", 42];
+tuple[0] = "world"; // Valid
+tuple[1] = 100; // Valid
+tuple[0] = 123; // Error: Type 'number' is not assignable to type 'string'.
+tuple[1] = "not a number"; // Error: Type 'string' is not assignable to type 'number'.  

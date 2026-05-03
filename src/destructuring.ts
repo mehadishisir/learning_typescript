83689c0970fc760c15e0aa8ea730dd308b7b72cb
mehadishisir <mehadishisir@gmail.com>
2026-05-03 11:43:00 +0600

    explore array destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -25,3 +25,18 @@ const {name:name1,age:age1,city:city1,address:{street,zip}} = personWithAddress
 // console.log(city1); // Output: "Dhaka"
 // console.log(street); // Output: "123 Main St"
 // console.log(zip); // Output: "12345"
+
+
+// array destructuring
+const numbers = [1, 2, 3, 4, 5]
+const [first, second, ...rest] = numbers
+// console.log(first); // Output: 1
+// console.log(second); // Output: 2
+// console.log(rest); // Output: [3, 4, 5]
+// nested array destructuring
+const nestedNumbers: [number, [number, number], number, number] = [1, [2, 3], 4, 5]
+const [first1, [second1, third1], ...rest1] = nestedNumbers
+// console.log(first1); // Output: 1
+// console.log(second1); // Output: 2
+// console.log(third1); // Output: 3
+// console.log(rest1); // Output: [4, 5]
